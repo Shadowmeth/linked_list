508fccdeccb6c87bdab3df8e9808dfac4991d06d
Shadowmeth <47725754+Shadowmeth@users.noreply.github.com>
2021-03-14 20:09:33 +0500

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-struct Node 
+struct Node
 {
 	int data;
 	Node* next;
@@ -10,8 +10,38 @@ struct Node
 struct List
 {
 	int sz; // size of the linked list
-	Node *first, *last;
+	Node* first, * last;
 };
+
+// PROTOTYPES
+List* newList();
+Node* newNode(int);
+Node* getNode(List*, int);
+Node* nextNode(Node*);
+Node* previousNode(List*, Node*);
+bool isLastNode(Node*);
+int size(List*);
+int indexOf(List*, int);
+bool isEmpty(List*);
+void insertAtBeginning(List*, int);
+void insertAtEnd(List*, int);
+void add(List*, int, int);
+void display(List*);
+List* reversedList(List*);
+bool listEqual(List*, List*);
+bool isPalindrome(List*);
+int frequency(List*, int);
+void displayFrequencies(List*);
+void reverseDisplay(List*);
+void deleteNodes(List*);
+void deleteList(List**);
+void remove(List*, int);
+List* duplicate(List*);
+void deleteSmallest(List*);
+int lastIndexOf(List*, int);
+void deleteDuplicates(List*);
+void printMiddle(List*);
+
 
 // return a new heap allocated linked list
 List* newList()
@@ -19,7 +49,7 @@ List* newList()
 	List* l = new List;
 	l->first = l->last = NULL;
 	l->sz = 0;
-	
+
 	return l;
 }
 
@@ -54,7 +84,8 @@ Node* nextNode(Node* n)
 }
 
 // return the previous node given the Node n
-Node* previousNode(List *l, Node* n)
+// if n itself is the first node we return the first node
+Node* previousNode(List* l, Node* n)
 {
 	Node* prev, * curr;
 	prev = curr = l->first;
@@ -97,8 +128,25 @@ int indexOf(List* l, int x)
 	return (temp == NULL) ? -1 : index;
 }
 
+// return last index of x in the linked list, -1 otherwise
+int lastIndexOf(List* l, int x)
+{
+	int index = -1;
+	Node* curr = l->first;
+	int i = 0;
+	while (curr) {
+		if (curr->data == x) {
+			index = i;
+		}
+		i++;
+		curr = curr->next;
+	}
+
+	return index;
+}
+
 // return true if list is empty
-bool isEmpty(List* l) 
+bool isEmpty(List* l)
 {
 	return (l->first == NULL) && (l->last == NULL);
 }
@@ -161,6 +209,56 @@ void add(List* l, int index, int item)
 	l->sz++;
 }
 
+void remove(List* l, int index)
+{
+	// if the index is greater than the size of linked list we can't do anything 
+	if (index > l->sz - 1) {
+		return; // do nothing, we received invalid index
+	}
+	if (index < 0) {
+		return; // index can't be negative either :)
+	}
+
+	Node* curr = l->first;
+	for (int i = 0; i < index; curr = curr->next, i++) {
+		; // do nothing
+	}
+
+	// curr now contains the node to be deleted
+	Node* prev = previousNode(l, curr);
+	// THERE ARE 2 special cases here, if index is 0 or the index is l->sz - 1
+	// we will need to update the first and last pointers appropriately
+	Node* temp = curr;
+	
+	if (curr == l->first) {
+		temp = l->first;
+		l->first = l->first->next;
+	}
+	else if (curr == l->last) {
+		temp = l->last;
+		l->last = previousNode(l, l->last);
+		l->last->next = NULL;
+	}
+	else {
+		prev->next = curr->next;
+	}
+
+	delete temp;
+	l->sz--; // DON'T FORGET TO UPDATE THE SIZE!!!
+}
+
+List* duplicate(List* l)
+{
+	List* duplicate = newList();
+	Node* curr = l->first;
+	while (curr) {
+		insertAtEnd(duplicate, curr->data);
+		curr = curr->next;
+	}
+
+	return duplicate;
+}
+
 // display the linked list
 void display(List* l)
 {
@@ -172,12 +270,12 @@ void display(List* l)
 }
 
 // this function returns a new list that is reversed
-List* reversedList(List *l)
+List* reversedList(List* l)
 {
-	List *reverse = newList();
+	List* reverse = newList();
 	// 5 -> 7 -> 17 -> 13 -> 11
 	// 11 -> 13 -> 17 -> 7 -> 5
-	Node *temp = l->first; // original list that we want to reverse
+	Node* temp = l->first; // original list that we want to reverse
 	while (temp) {
 		insertAtBeginning(reverse, temp->data);
 		temp = temp->next;
@@ -186,15 +284,15 @@ List* reversedList(List *l)
 	return reverse;
 }
 
-// we want to check if the linked lists are empty or not
-bool listEqual(List *l1, List *l2)
+// we want to check if the linked lists are equal or not
+bool listEqual(List* l1, List* l2)
 {
 	if (l1->sz != l2->sz) {
 		return false;
 	}
 
-	Node *temp1 = l1->first;
-	Node *temp2 = l2->first;
+	Node* temp1 = l1->first;
+	Node* temp2 = l2->first;
 	while (temp1 && temp2) {
 		if (temp1->data != temp2->data) {
 			return false;
@@ -207,17 +305,20 @@ bool listEqual(List *l1, List *l2)
 }
 
 // return whether the list is a palindrome or not
-bool isPaldindrome(List *l)
+bool isPalindrome(List* l)
 {
-	List *reversed = reversedList(l);
+	List* reversed = reversedList(l);
 
-	return listEqual(l, reversed);
+	bool isLPalindrome = listEqual(l, reversed);
+	deleteList(&reversed);
+
+	return isLPalindrome;
 }
 
-int frequency(List *l, int element)
+int frequency(List* l, int element)
 {
 	int count = 0;
-	Node *temp = l->first;
+	Node* temp = l->first;
 	while (temp) {
 		if (temp->data == element) {
 			count++;
@@ -227,12 +328,12 @@ int frequency(List *l, int element)
 	return count;
 }
 
-void displayFrequencies(List *l)
+void displayFrequencies(List* l)
 {
 	// we can maintain a new linked list
 	// one that contains the elements whose frequency has been displayed
-	List *displayed = newList();
-	Node *temp = l->first;
+	List* displayed = newList();
+	Node* temp = l->first;
 	while (temp) {
 		int d = temp->data;
 		if (indexOf(displayed, d) == -1) {
@@ -244,25 +345,96 @@ void displayFrequencies(List *l)
 
 		temp = temp->next;
 	}
-
+	deleteList(&displayed);
 }
 
-void deleteNodes(List *l)
+void deleteSmallest(List* l)
+{
+	if (l->sz == 0) {
+		return; // do nothing, list is empty
+	}
+
+	// we can find the index of the smallest element and pass l and the index to remove()
+	Node* curr = l->first->next;
+	int smallest = l->first->data;
+
+	while (curr) {
+		if (curr->data < smallest) {
+			smallest = curr->data;
+		}
+
+		curr = curr->next;
+	}
+
+	remove(l, indexOf(l, smallest));
+}
+
+void deleteDuplicates(List* l)
+{
+	Node* curr = l->first;
+	while (curr) {
+		while (frequency(l, curr->data) > 1) {
+			remove(l, lastIndexOf(l, curr->data));
+		}
+		curr = curr->next;
+	}
+}
+
+void deleteNodes(List* l)
 {
 	// we need to delete on every node
-	Node *temp = l->first;
+	Node* temp = l->first;
 	while (temp) {
-		Node *n = temp;
+		Node* n = temp;
 		temp = temp->next;
 		delete n;
 	}
 }
 
-void deleteList(List **l)
+void deleteList(List** l)
 {
 	deleteNodes(*l);
-	delete *l;
+	delete* l;
 	*l = NULL;
+}
+
+void reverseDisplay(List* l)
+{
+
+	if (l->first == NULL && l->last == NULL) {
+		// list is empty
+		return; // do nothing
+	}
+	else if (l->first == l->last) {
+		// list contains only 1 element, display it and exit
+		cout << l->first->data << endl;
+		return;
+	}
+	else {
+		// list contains multiple elements, we need to display them all in reverse order
+		Node* curr;
+		Node* stop = NULL;
+		while (stop != l->first) {
+			curr = l->first;
+			while (curr->next != stop) {
+				curr = curr->next;
+			}
+			cout << curr->data << " ";
+			stop = curr;
+		}
+
+		cout << endl;
+	}
+}
+
+void printMiddle(List* l)
+{
+	Node* curr = l->first;
+	for (int i = 1; i <= l->sz / 2; i++) {
+		curr = curr->next;
+	}
+
+	cout << "Middle is: " << curr->data << endl;
 }
 
 int main()
@@ -277,12 +449,8 @@ int main()
 	insertAtEnd(l, 18);
 	insertAtEnd(l, 5);
 	insertAtEnd(l, 18);
-	display(l);
+	
 
-	displayFrequencies(l);
 
-	// all the Node and List objects are allocated on heap
-	// we need to delete them also
 	deleteList(&l);
-	cout << l << endl;
 }
